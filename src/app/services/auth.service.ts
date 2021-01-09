153210af0fee7a75679d3b66ff28d761f41335e3
Juan Belda Molina <juan.belda@alicante.es>
2021-01-09 09:07:35 +0100

    Tema 79. Obtener información del usuario activo en FireBase

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -8,6 +8,15 @@ export class AuthService {
 
   constructor(public auth: AngularFireAuth) { }
 
+  initAuthListener() {
+    this.auth.authState.subscribe( fbUser => {
+      console.log(fbUser);
+      console.log(fbUser?.uid);
+      console.log(fbUser?.email);
+      console.log(fbUser?.emailVerified);
+    })
+  }
+
   crearUsuario(nombre: string, email: string, password: string) {
     return this.auth.createUserWithEmailAndPassword(email, password);
   }

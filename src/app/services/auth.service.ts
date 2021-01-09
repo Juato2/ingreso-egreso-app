5378226bfdf6a39dbb52fb7a65fbac3f61ab717b
Juan Belda Molina <juan.belda@alicante.es>
2021-01-09 10:01:42 +0100

    Tema 81. Crear documento de base de datos por cada usuario

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,25 +1,34 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { map } from 'rxjs/operators';
+import { Usuario } from '../models/usuario.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
-  constructor(public auth: AngularFireAuth) { }
+  constructor(
+    public auth: AngularFireAuth,
+    private firestore: AngularFirestore
+  ) {}
 
   initAuthListener() {
-    this.auth.authState.subscribe( fbUser => {
+    this.auth.authState.subscribe((fbUser) => {
       console.log(fbUser);
       console.log(fbUser?.uid);
       console.log(fbUser?.email);
       console.log(fbUser?.emailVerified);
-    })
+    });
   }
 
   crearUsuario(nombre: string, email: string, password: string) {
-    return this.auth.createUserWithEmailAndPassword(email, password);
+    return this.auth
+      .createUserWithEmailAndPassword(email, password)
+      .then(({ user }) => {
+        const newUser = new Usuario(user.uid, nombre, user.email);
+        return this.firestore.doc(`${user.uid}/usuario`).set({ ...newUser });
+      });
   }
 
   loginUsuario(email: string, password: string) {
@@ -31,8 +40,6 @@ export class AuthService {
   }
 
   isAuth() {
-    return this.auth.authState.pipe(
-      map( fbUser => fbUser != null )
-    );
+    return this.auth.authState.pipe(map((fbUser) => fbUser != null));
   }
 }

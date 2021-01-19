c6a3ba3e6867e531d7c9bd4c2b1943c43ebb04af
Juan Belda <juato2@gmail.com>
2021-01-19 14:45:50 +0100

    Tema 120. LazyLoad del módulo de ingreso y egreso (NOTA: corregidas las comillas dobles)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,8 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
-import { LoginComponent } from "./auth/login/login.component";
-import { RegisterComponent } from "./auth/register/register.component";
-import { DashboardComponent } from "./dashboard/dashboard.component";
-import { dashboardRoutes } from "./dashboard/dashboard.routes";
-import { AuthGuard } from './services/auth.guard';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+
 
 const routes: Routes = [
 

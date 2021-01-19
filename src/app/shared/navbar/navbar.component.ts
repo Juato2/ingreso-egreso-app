6f7887813337d0b4fb6f5df3ab7c265258b049b1
Juan Belda <juato2@gmail.com>
2021-01-19 13:10:40 +0100

    Sección 9 lista

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
+import { AppState } from 'src/app/app.reducer';
 
 @Component({
   selector: 'app-navbar',
@@ -6,11 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
+  nombre = '';
+  userSubscription: Subscription;
 
-  constructor() { }
+  constructor(private store: Store<AppState>) { }
+
+  ngOnDestroy(): void {
+    this.userSubscription.unsubscribe();
+  }
 
   ngOnInit(): void {
+    this.userSubscription = this.store.select('user')
+      .pipe(
+        filter((auth) => {
+          return auth.user !== null;
+        })
+      )
+      .subscribe(({ user }) => this.nombre = user.nombre);
   }
 
 }

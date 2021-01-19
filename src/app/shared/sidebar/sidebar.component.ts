7ff161a1137caa7487aa58cde7ea85f62fe8dbf7
Juan Belda <juato2@gmail.com>
2021-01-19 13:05:32 +0100

    Tema 111. Colocar el nombre del usuario activo

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,18 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { Router } from '@angular/router';
+import { AppState } from 'src/app/app.reducer';
+import { Store } from '@ngrx/store';
+import { filter } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styles: [],
 })
-export class SidebarComponent implements OnInit {
-  constructor(private authService: AuthService, private router: Router) {}
+export class SidebarComponent implements OnInit, OnDestroy {
+  nombre = '';
+  userSubscription: Subscription;
 
-  ngOnInit(): void {}
+  constructor(private authService: AuthService, private router: Router, private store: Store<AppState>) { }
 
-  cerrarSesion() {
+  ngOnDestroy(): void {
+    this.userSubscription.unsubscribe();
+  }
+
+  ngOnInit(): void {
+    this.userSubscription = this.store.select('user')
+      .pipe(
+        filter((auth) => {
+          return auth.user !== null;
+        })
+      )
+      .subscribe(({ user }) => this.nombre = user.nombre);
+  }
+
+  cerrarSesion(): void {
     this.authService.logout().then(() => {
       this.router.navigate(['/login']);
     });
